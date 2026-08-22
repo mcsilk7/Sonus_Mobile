c1cfbb5f199e9c925bc4b75d5f1ee6e348907d04
mcsilk <kubadolak7@gmail.com>
2026-08-22 18:14:24 +0200

    v2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.sonus"
         minSdk = 24
         targetSdk = 37
-        versionCode = 2
-        versionName = "2.0.1"
+        versionCode = 3
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
